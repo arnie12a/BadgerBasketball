251fa33b7c2615afcf300f197b82ee6907e2ab5e
Arnav <arnav.n.karnik@gmail.com>
2025-05-27 23:07:08 -0700

    creating the dashboard with streamlit

--- app.py
+++ app.py
@@ -1,24 +1,103 @@
 import streamlit as st
+import pandas as pd
 
-# Title of the app
-st.title("My First Streamlit App")
+def AboutPlayers(df):
+    cols = ['Player', '#', 'Class', 'Height', 'Weight', 'Summary']
+    return df[cols]
+    
 
-# Header
-st.header("Welcome to the Streamlit App")
+def EachGameStatistics(df):
+    return
 
-# Subheader
-st.subheader("This is a basic example")
+def PerGamePlayerStatistics(df):
+    return
 
-# Text
-st.write("Streamlit makes it easy to create web apps for data science and machine learning.")
+def PerGameTeamStatistics(df):
+    return
 
-# Input widget
-name = st.text_input("Enter your name:")
+def getData(year):
 
-# Button
-if st.button("Submit"):
-    st.success(f"Hello, {name}! 👋")
+    aboutPlayers = pd.read_csv('./data/about_players/about_players_' + year + '.csv')
+    aboutPlayers = aboutPlayers[aboutPlayers['seasonYear'] == int(year)]
+    aboutPlayers = AboutPlayers(aboutPlayers)
 
-# Sidebar
-st.sidebar.title("Sidebar")
-st.sidebar.write("You can add filters or options here.")
+    eachGameStats = pd.read_csv('./data/each_game_stats/each_game_stats_' + year + '.csv')
+    eachGameStats = eachGameStats[eachGameStats['seasonYear'] == int(year)]
+
+    perGamePlayerStats = pd.read_csv('./data/per_game_player_stats/per_game_player_stats' + year + '.csv')
+    perGamePlayerStats = perGamePlayerStats[perGamePlayerStats['seasonYear'] == int(year)]
+
+    perGameTeamStats = pd.read_csv('./data/per_game_team_stats/per_game_team_stats_' + year + '.csv')
+    perGameTeamStats = perGameTeamStats[perGameTeamStats['seasonYear'] == int(year)]
+
+    return aboutPlayers, eachGameStats, perGamePlayerStats, perGameTeamStats
+
+# Sidebar title
+st.sidebar.title("Badger Basketball Analysis")
+
+# Dropdown with 6 options
+option = st.sidebar.selectbox(
+    "Select a year:",
+    ("2020", "2021", "2022", "2023", "2024", "2025")
+)
+
+# Conditional inputs based on option
+if option == "2020":
+    pass
+elif option == "2021":
+    pass
+elif option == "2022":
+    pass
+elif option == "2023":
+    pass
+elif option == "2024":
+    pass
+elif option == "2025":
+    pass
+
+# Submit button
+submit = st.sidebar.button("Submit")
+
+# Display dummy output
+if submit:
+    if option == "2020":
+        df1, df2, df3, df4 = getData(option)
+        st.dataframe(df1)
+        st.dataframe(df2)
+        st.dataframe(df3)
+        st.dataframe(df4)
+
+    elif option == "2021":
+        df1, df2, df3, df4 = getData(option)
+        st.dataframe(df1)
+        st.dataframe(df2)
+        st.dataframe(df3)
+        st.dataframe(df4)
+
+    elif option == "2022":
+        df1, df2, df3, df4 = getData(option)
+        st.dataframe(df1)
+        st.dataframe(df2)
+        st.dataframe(df3)
+        st.dataframe(df4)
+
+    elif option == "2023":
+        df1, df2, df3, df4 = getData(option)
+        st.dataframe(df1)
+        st.dataframe(df2)
+        st.dataframe(df3)
+        st.dataframe(df4)
+
+    elif option == "2024":
+        df1, df2, df3, df4 = getData(option)
+        st.dataframe(df1)
+        st.dataframe(df2)
+        st.dataframe(df3)
+        st.dataframe(df4)
+
+    elif option == "2025":
+        df1, df2, df3, df4 = getData(option)
+        st.dataframe(df1)
+        st.dataframe(df2)
+        st.dataframe(df3)
+        st.dataframe(df4)
